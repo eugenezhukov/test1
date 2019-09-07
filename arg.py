d167a68807d2dd3ac58127144c43efd9ed94249f
eugenezhukov <eugene.zhukov@gmail.com>
2019-09-07 22:13:00 +0300

    New example argv 2

--- arg.py
+++ arg.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
-import sys
 from sys import argv
 
 if len(argv)<2:
     print ("No any argument passed. Exiting.")
-    sys.exit()
 else:
     print ('Command line',argv[0])
     interface = argv[1]
