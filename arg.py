ff5354aec6aee874d40962f660cf06e0ef3bfd69
eugenezhukov <eugene.zhukov@gmail.com>
2019-09-07 22:09:24 +0300

    New example argv 2

--- arg.py
+++ arg.py
@@ -5,8 +5,8 @@ from sys import argv
 if len(argv)<2:
     print ("No any argument passed. Exiting.")
     sys.exit()
-
-print ('Command line',argv[0])
-interface = argv[1]
-vlan = argv[2]
-print (interface,vlan)
+else:
+    print ('Command line',argv[0])
+    interface = argv[1]
+    vlan = argv[2]
+    print (interface,vlan)
